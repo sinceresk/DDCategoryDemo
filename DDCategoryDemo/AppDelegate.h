4fed3af8aa8f44d419673b22eb418996d1286e68
Daredos <fenglh@Daredos.local>
2015-12-10 16:10:00 +0800

    Initial Commit

---
+++ DDCategoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDCategoryDemo
+//
+//  Created by Daredos on 15/12/10.
+//  Copyright (c) 2015年 BlueMoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDCategoryDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DDCategoryDemo
+//
+//  Created by Daredos on 15/12/10.
+//  Copyright (c) 2015年 BlueMoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
